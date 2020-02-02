f6caad732ed1f8196a40eae70ccbc0f09fc6d3c7
tekinomikata <tekinomikata@outlook.com>
2020-02-02 10:29:15 -0300

    Added say command

---
+++ src/Rhisis.World/Game/Chat/Commands/SayCommand.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using Rhisis.Core.Common;
+using Rhisis.World.Game.Entities;
+using Rhisis.World.Packets;
+
+namespace Rhisis.World.Game.Chat.Commands
+{
+    [ChatCommand("/say", AuthorityType.Player)]
+    [ChatCommand("/s", AuthorityType.Player)]
+    public class SayChatCommand : IChatCommand
+    {
+        private readonly ILogger<SayChatCommand> _logger;
+        private readonly IChatPacketFactory _chatPacketFactory;
+        private readonly ITextPacketFactory _textPacketFactory;
+        private readonly IWorldServer _worldServer;
+
+        /// <summary>
+        /// Creates a new <see cref="SayChatCommand"/> instance.
+        /// </summary>
+        /// <param name="logger">Logger.</param>
+        /// <param name="chatPacketFactory"></param>
+        /// <param name="textPacketFactory"></param>
+        /// <param name="worldServer"></param>
+        public SayChatCommand(ILogger<SayChatCommand> logger, IChatPacketFactory chatPacketFactory, ITextPacketFactory textPacketFactory, IWorldServer worldServer)
+        {
+            _logger = logger;
+            _chatPacketFactory = chatPacketFactory;
+            _textPacketFactory = textPacketFactory;
+            _worldServer = worldServer;
+        }
+
+        /// <inheritdoc />
+        public void Execute(IPlayerEntity player, object[] parameters)
+        {
+            if (parameters.Length > 1)
+            {
+                _chatPacketFactory.SendChat(player, string.Join(" ", parameters));
+                return;
+            }
+
+            string playerName = (string)parameters[0];
+            IPlayerEntity playerLookup = _worldServer.GetPlayerEntity(playerName);
+            _textPacketFactory.SendSnoop(player, playerLookup == null
+                ? $"{playerName} is not online."
+                : $"{playerName} is online.");
+        }
+    }
+}
